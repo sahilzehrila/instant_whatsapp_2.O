72b711a0744604b5c1e75f3f75150aedf8413e88
sahilzehrila <131785548+sahilzehrila@users.noreply.github.com>
2025-04-15 15:36:08 +0530

    Update Navbar.js name change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="navbar navbar-dark bg-success shadow">
       <div className="container-fluid">
         <span className="navbar-brand mb-0 h1">
-          <i className="bi bi-whatsapp"></i> Rapid Whatsapp
+          <i className="bi bi-whatsapp"></i> Instant Whatsapp
         </span>
         <span>
           <a
